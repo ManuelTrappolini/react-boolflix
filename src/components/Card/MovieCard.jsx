9eed832776ea9b8778ee66fd9b24cb191fc2feb8
ManuelTrappolini <manuel.trappolini@gmail.com>
2024-11-29 13:01:20 +0100

    add icons for movie and tvshows rating

--- src/components/Card/MovieCard.jsx
+++ src/components/Card/MovieCard.jsx
@@ -1,4 +1,7 @@
 import { IT, US, FR, ES, JP, GB, DE, CN, KR, RU } from 'country-flag-icons/react/3x2'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
 
 export default function MovieCard({ movies }) {
     console.log("MovieCard ricevuto:", movies);
@@ -29,7 +32,15 @@ export default function MovieCard({ movies }) {
                         <li><h4 >Movie Title:</h4> {movies?.title}</li>
                         <li><h4>Original Title:</h4> {movies?.original_title}</li>
                         <li><h4>Original Language:</h4> <FlagIcon className="movie-flag" /></li>
-                        <li><h4>Average Vote: </h4> {movies?.vote_average}</li>
+                        <li><h4>Average Vote: </h4>
+                            {movies.vote_average <= 2 ? <span className='rating-icon'><FontAwesomeIcon icon={faStar} /></span>
+                                : movies.vote_average <= 4 ? <span className='rating-icon'><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /></span>
+                                    : movies.vote_average <= 6 ? <span className='rating-icon'><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /> </span>
+                                        : movies.vote_average <= 8 ? <span className='rating-icon'><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /></span>
+                                            : <span className='rating-icon'><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /></span>
+                            }
+                        </li>
+
                         <li><img src={`http://image.tmdb.org/t/p/w300/${movies.poster_path}`} alt="" /></li>
                         <hr />
                     </ul>
--- src/components/Card/TvShows.jsx
+++ src/components/Card/TvShows.jsx
@@ -1,4 +1,6 @@
 import { IT, US, FR, ES, JP, GB, DE, CN, KR, RU } from 'country-flag-icons/react/3x2'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 export default function TvShowsCard({ tvShows }) {
 
@@ -28,8 +30,19 @@ export default function TvShowsCard({ tvShows }) {
                         <li><h4 >TvShows Title:</h4> {tvShows.name}</li>
                         <li><h4>Original Title:</h4> {tvShows.original_name}</li>
                         <li><h4>Original Language:</h4> <FlagIcon className="movie-flag" /></li>
-                        <li><h4>Average Vote: </h4> {tvShows.vote_average}</li>
-                        <li><img src={`http://image.tmdb.org/t/p/w300/${tvShows.poster_path}`} alt="" /></li>
+
+                        <li><h4>Average Vote: </h4>
+                            {tvShows.vote_average <= 2 ? <span className='rating-icon'><FontAwesomeIcon icon={faStar} /></span>
+                                : tvShows.vote_average <= 4 ? <span className='rating-icon'><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /></span>
+                                    : tvShows.vote_average <= 6 ? <span className='rating-icon'><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /> </span>
+                                        : tvShows.vote_average <= 8 ? <span className='rating-icon'><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /></span>
+                                            : <span className='rating-icon'><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /><FontAwesomeIcon icon={faStar} /></span>
+                            }
+
+
+
+                        </li>
+                        <li><img src={`http://image.tmdb.org/t/p/w342/${tvShows.poster_path}`} alt="" /></li>
                         <hr />
                     </ul>
                 </div>
